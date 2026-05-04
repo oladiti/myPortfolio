60528a74804bbc6d1e7d231d7022e3eb069b48aa
oladiti <oladitiabiodun86@gmail.com>
2026-05-04 12:57:46 +0100

    adjusting the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
   }, [theme]);
 
   return (
-    <div className={`container ${theme}  max-w-screen-2xl  overflow-hidden md:ml-[200px]`}>
+    <div className={`container ${theme}  max-w-screen-2xl overflow-hidden md:ml-[100px]`}>
       <Navbar theme={theme} setTheme={setTheme} />
       <AnimatedComponent>
         <Home />
